76cdf868332db690c636a84f4c97aedf150f15c5
Hugo Mendes <hugomoraismendes@gmail.com>
2021-09-28 22:11:13 -0300

    first commit

---
+++ src/send-message.js
@@ -0,0 +1,27 @@
+const AWS = require("aws-sdk");
+AWS.config.update({
+  region: "us-east-1",
+});
+const sqs = new AWS.SQS();
+
+(async () => {
+  await sqs
+    .sendMessage({
+      MessageBody: JSON.stringify({
+        conta_origem: {
+          agencia: 0001,
+          numero_conta: "123456-7",
+        },
+        conta_destino: {
+          agencia: 0001,
+          numero_conta: "765432-1",
+        },
+        valor: 1000,
+        moeda: "BRL",
+      }),
+      QueueUrl:
+        "https://sqs.us-west-2.amazonaws.com/038465640849/fila-teste-hugo",
+    })
+    .promise();
+  console.log("mensagem enviada com sucesso!");
+})();
